effe6169a0f44ce37ed4e7672a02244fa65070fa
nshumoogum <nathan.shumoogum@methods.co.uk>
2020-01-28 09:40:42 +0000

    Refactored edit-logs script
    * Rework usage function and use when calling h flag
    * Send error text to stderr (convention)
    * Remove unnecessary quotes
    * Remove unnecessary use of backslash to escape characters in regex
    * Apply idiomatic bash for if statements
    * Update success message to done as there is no check to see if script ran successfully on completion

--- scripts/edit-logs.sh
+++ scripts/edit-logs.sh
@@ -1,12 +1,31 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-path=''
-directory='false'
+path=
+directory=false
 
 print_usage() {
-  echo "Sctipt is to help replace go-ns logger to log.go logger. This script will update the package in use and attempt to refactor current logs into the new structure."
-  echo "This script cannot handlelogs across multiple lines."
-  echo "Usage: ./edit-logs.sh -d -p /go-projects/search-api/handlers"
+  echo ""
+  echo "Update Logger Script"
+  echo ""
+  echo ""
+  echo "Script is to help replace go-ns logging with log.go logging. This script will update the package in use and attempt to refactor current logging into the new structure."
+  echo ""
+  echo " * Updates logging library in file or directory"
+  echo " * Attempts to update as many variations of old logs to new logs"
+  echo " * Script is unable to update logs which traverse multiple lines"
+  echo ""
+  echo "Options are:"
+  echo ""
+  echo "      OPTION    DESCRIPTION                                 EXAMPLE ('' does NOT indicate default value)"
+  echo "        -p      The path to file or directory from \$HOME    \"go/src/github.com/myNewService\""
+  echo "        -d      Boolean flag, set if path is to directory   No value is needed, it defaults to false if not set"
+  echo "        -h      Help                                        No value is needed, returns description of flags"
+  echo ""
+  echo "Examples:"
+  echo ""
+  echo " * ./edit-logs.sh -d -p /go-projects/search-api/handlers"
+  echo " * ./edit-logs.sh -p /go-projects/search-api/handlers/handlers.go"
+  echo ""
 }
 
 while getopts 'hdp:' flag; do
@@ -15,67 +34,59 @@ while getopts 'hdp:' flag; do
     ;;
     d) directory='true'
     ;;
-    h)
-      echo ""
-      echo "Update Logger Script"
-      echo ""
-      echo " * Updates logging library in file or directory"
-      echo " * Attempts to update as many variations of old logs to new logs"
-      echo " * Script is unable to update logs which traverse multiple lines"
-      echo ""
-      echo "Options are:"
-      echo ""
-      echo "      OPTION    DESCRIPTION                                 EXAMPLE ('' does NOT indicate default value)"
-      echo "        -p      The path to file or directory from \$HOME    \"go/src/github.com/myNewService\""
-      echo "        -d      Boolean flag, set if path is to directory   No value is needed, it defaults to false if not set"
-      echo "        -h      Help                                        No value is needed, returns description of flags"
-      echo ""
-      exit 0
+    h) print_usage
+       exit 0
     ;;
-    \?)
-      echo "ERROR: Unknown option $OPTARG"
+    \?) echo "ERROR: Unknown option $OPTARG" >&2
+        echo "use -h flag for help"
+        exit 1
     ;;
-    *) print_usage
+    *) print_usage >&2
        exit 1
        ;;
   esac
 done
 
-home="$HOME"
+home=$HOME
 
-path=${path:?ERROR: var not set [-p path]}
+if [[ -z $path ]]; then
+  echo "ERROR: var not set [-p path]" >&2
+  exit 1
+fi
+
+# path=${path:?ERROR: var not set [-p path]}
 
 update_file(){
     echo "update ${1}"
 
     echo "Update log library"
     echo "=============================================="
-    perl -i -p -e 's/github.com\/ONSdigital\/go-ns\/log/github.com\/ONSdigital\/log.go\/log/g' $1
-    echo "success"
+    perl -i -p -e 's!github.com/ONSdigital/go-ns/log!github.com/ONSdigital/log.go/log!g' $1
+    echo "done"
 
 
     echo "Capture and replace logs with err set"
     echo "=============================================="
-    perl -i -p -e 's/log\.(?:ErrorCtx|ErrorC|Error)\((?:ctx, )?(\"[^"]+\")?(?:, )?(?:err)(?:\, nil)*(?:, )*(\, logData|\, log\.Data\{\"[^"]+\": .+)*}?\)/log\.Event\(ctx\, ${1}, log\.Error\(err\)${2})/g' $1
-    echo "success"
+    perl -i -p -e 's/log.(?:ErrorCtx|ErrorC|Error)\((?:ctx, )?("[^"]+"||[A-Za-z)-9]+)?(?:, )?(?:err)(?:, nil)*(?:, )*(, logData|, log.Data{"[^"]+": .+)*}?\)/log.Event(ctx, ${1}, log.Error(err)${2})/g' $1
+    echo "done"
 
 
     echo "Capture and replace error logs with log.Data containing error"
     echo "=============================================="
-    perl -i -p -e 's/log\.(?:ErrorCtx|InfoCtx|DebugCtx|Error|Debug|Info)\((?:ctx, )*(\"[^"]+\")*(?:nil)*(?:, )*(log\.Data\{(\"[^"]+\": .+?, )?(\"error\": err)(?:, )?(\"[^"]+\": .+)*})\)/log\.Event\(ctx\, ${1}, log\.Error\(err\)\, log\.Data\{${3}${5}\}\)/g' $1
-    echo "success"
+    perl -i -p -e 's/log.(?:ErrorCtx|InfoCtx|DebugCtx|Error|Debug|Info)\((?:ctx, )*("[^"]+")*(?:nil)*(?:, )*(log.Data{("[^"]+": .+?, )?("error": err)(?:, )?("[^"]+": .+)*})\)/log.Event(ctx, ${1}, log.Error(err), log.Data{${3}${5}})/g' $1
+    echo "done"
 
 
     echo "Capture and replace logs with log.Data or logData or not at all if both missing (handles nil)"
     echo "=============================================="
-    perl -i -p -e 's/log\.(?:ErrorCtx|InfoCtx|DebugCtx|Error|Debug|Info)\((?:ctx, )?(\"[^"]+\")*(?:nil)*(?:, )*(?:nil)?(logData|log\.Data\{\"[^"]+\": .+)*}?\)/log\.Event\(ctx\, ${1}, ${2}\)/g' $1
+    perl -i -p -e 's/log.(?:ErrorCtx|InfoCtx|DebugCtx|Error|Debug|Info)\((?:ctx, )?("[^"]+")*(?:nil)*(?:, )*(?:nil)?(logData|log.Data\{"[^"]+": .+)*}?\)/log.Event(ctx, ${1}, ${2})/g' $1
     echo "finished updating file: $1"
 }
 
-if [ $path != "" ]; then
+if [[ -n $path ]]; then
     fullpath=$home$path
       
-    if [ "$directory" == "true" ]; then
+    if [[ $directory == true ]]; then
       echo "path to directory: ${fullpath}"
       for filename in "$fullpath"/*.go; do
         update_file $filename
@@ -84,5 +95,5 @@ if [ $path != "" ]; then
       update_file $fullpath
     fi
 
-    echo "success\n"
+    echo "done\n"
 fi
