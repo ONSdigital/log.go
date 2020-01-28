2c3acf305a6a49cdc8f0f48bd9b323ae078417c2
Geraint Edwards <gedgey@gmail.com>
2020-01-28 16:16:55 +0000

    fix indentation, remove unnecessary -d flag (figure it out automagically)

--- scripts/edit-logs.sh
+++ scripts/edit-logs.sh
@@ -1,99 +1,91 @@
 #!/usr/bin/env bash
 
 path=
-directory=false
+me=$(basename $0)
 
 print_usage() {
-  echo ""
-  echo "Update Logger Script"
-  echo ""
-  echo ""
-  echo "Script is to help replace go-ns logging with log.go logging. This script will update the package in use and attempt to refactor current logging into the new structure."
-  echo ""
-  echo " * Updates logging library in file or directory"
-  echo " * Attempts to update as many variations of old logs to new logs"
-  echo " * Script is unable to update logs which traverse multiple lines"
-  echo ""
-  echo "Options are:"
-  echo ""
-  echo "      OPTION    DESCRIPTION                                 EXAMPLE ('' does NOT indicate default value)"
-  echo "        -p      The path to file or directory from \$HOME    \"go/src/github.com/myNewService\""
-  echo "        -d      Boolean flag, set if path is to directory   No value is needed, it defaults to false if not set"
-  echo "        -h      Help                                        No value is needed, returns description of flags"
-  echo ""
-  echo "Examples:"
-  echo ""
-  echo " * ./edit-logs.sh -d -p /go-projects/search-api/handlers"
-  echo " * ./edit-logs.sh -p /go-projects/search-api/handlers/handlers.go"
-  echo ""
+    echo ""
+    echo "Update Logger Script"
+    echo ""
+    echo ""
+    echo "$me helps replace go-ns logging with log.go logging. This script will update the package in use and attempt to refactor current logging into the new structure."
+    echo ""
+    echo " * Updates logging library in given file or directory"
+    echo " * Attempts to update as many variations of old logs to new logs"
+    echo " * Unable to update logs which traverse multiple lines"
+    echo ""
+    echo "      OPTION              DESCRIPTION         EXAMPLE (NOT default value)"
+    echo "        -p <file_or_dir>  File or directory   \"go/src/github.com/myNewService\""
+    echo "        -h                Show this help"
+    echo ""
+    echo "Example usage:"
+    echo ""
+    echo " * Add the script to your PATH, with one of:"
+    echo "   * PATH=\$GOPATH/src/github.com/ONSdigital/log.go/scripts:\$PATH"
+    echo "   * cp -ip scripts/$me ~/bin/$me"
+    echo " * Run against chosen file or directory:"
+    echo "   * $me -p handlers/handlers.go"
+    echo "   * $me -p handlers"
+    echo ""
 }
 
-while getopts 'hdp:' flag; do
-  case "${flag}" in
-    p) path="${OPTARG}"
-    ;;
-    d) directory='true'
-    ;;
-    h) print_usage
-       exit 0
-    ;;
-    \?) echo "ERROR: Unknown option $OPTARG" >&2
-        echo "use -h flag for help"
-        exit 1
-    ;;
-    *) print_usage >&2
-       exit 1
-       ;;
-  esac
+while getopts 'hp:' flag; do
+    case "${flag}" in
+        (p) path="${OPTARG}"
+            ;;
+        (h) print_usage
+            exit 0
+            ;;
+       (\?) echo "ERROR: Unknown option $OPTARG" >&2
+            echo "use -h flag for help"
+            exit 1
+            ;;
+        (*) print_usage >&2
+            exit 1
+            ;;
+    esac
 done
 
-home=$HOME
-
 if [[ -z $path ]]; then
-  echo "ERROR: var not set [-p path]" >&2
+  echo "ERROR: required flag not used [-p path]" >&2
   exit 1
 fi
 
-# path=${path:?ERROR: var not set [-p path]}
+banner() {
+    echo "$@"
+    echo "===================================================="
+}
 
 update_file(){
-    echo "update ${1}"
+    banner "update ${1}"
 
-    echo "Update log library"
-    echo "=============================================="
+    banner "Update log library"
     perl -i -p -e 's!github.com/ONSdigital/go-ns/log!github.com/ONSdigital/log.go/log!g' $1
     echo "done"
 
 
-    echo "Capture and replace logs with err set"
-    echo "=============================================="
+    banner "Capture and replace logs with err set"
     perl -i -p -e 's/log.(?:ErrorCtx|ErrorC|Error)\((?:ctx, )?("[^"]+"||[A-Za-z)-9]+)?(?:, )?(?:err)(?:, nil)*(?:, )*(, logData|, log.Data{"[^"]+": .+)*}?\)/log.Event(ctx, ${1}, log.Error(err)${2})/g' $1
     echo "done"
 
 
-    echo "Capture and replace error logs with log.Data containing error"
-    echo "=============================================="
+    banner "Capture and replace error logs with log.Data containing error"
     perl -i -p -e 's/log.(?:ErrorCtx|InfoCtx|DebugCtx|Error|Debug|Info)\((?:ctx, )*("[^"]+")*(?:nil)*(?:, )*(log.Data{("[^"]+": .+?, )?("error": err)(?:, )?("[^"]+": .+)*})\)/log.Event(ctx, ${1}, log.Error(err), log.Data{${3}${5}})/g' $1
     echo "done"
 
 
-    echo "Capture and replace logs with log.Data or logData or not at all if both missing (handles nil)"
-    echo "=============================================="
+    banner "Capture and replace logs with log.Data or logData or not at all if both missing (handles nil)"
     perl -i -p -e 's/log.(?:ErrorCtx|InfoCtx|DebugCtx|Error|Debug|Info)\((?:ctx, )?("[^"]+")*(?:nil)*(?:, )*(?:nil)?(logData|log.Data\{"[^"]+": .+)*}?\)/log.Event(ctx, ${1}, ${2})/g' $1
     echo "finished updating file: $1"
 }
 
-if [[ -n $path ]]; then
-    fullpath=$home$path
-      
-    if [[ $directory == true ]]; then
-      echo "path to directory: ${fullpath}"
-      for filename in "$fullpath"/*.go; do
+if [[ -d $path ]]; then
+    banner "path to directory: ${path}"
+    for filename in "$path"/*.go; do
         update_file $filename
-      done
-    else
-      update_file $fullpath
-    fi
-
-    echo "done\n"
+    done
+else
+    update_file $path
 fi
+
+banner "done"
